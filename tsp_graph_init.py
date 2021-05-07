576fb020f85dccf3e41199b8eb08866d94444f16
Amaury Bonneau <amaury.bonneau@isen-ouest.yncrea.fr>
2021-05-07 14:46:33 +0200

    calcul des distances dans Route via la matrice, ajout de commentaires et vérifications

--- tsp_graph_init.py
+++ tsp_graph_init.py
@@ -1,4 +1,7 @@
 from math import sqrt
+import random
+import numpy as np
+import pandas as pd
 
 class Lieu ():
     # Classe de création de lieux entendus comme des points de coordonnées x et y
@@ -7,6 +10,9 @@ class Lieu ():
         self.x = x
         self.y = y
     
+    def __repr__(self):
+        return self.__dict__
+
     def calcul_distance(self, point):
         """Calcul de la distance entre le point self et une autre instance de Lieu appelée point."""
         distance = sqrt((self.x-point.x)**2 +(self.y-point.y)**2)
@@ -14,18 +20,32 @@ class Lieu ():
 
 
 class Route ():
-    def __init__(self, ordre):
+    def __init__(self, ordre, matrice):
         """Instanciation d'une route, ordre est une liste de points (objets Lieu)"""
-        self.ordre = ordre        
-    
-    def calcul_distance_route(self):
-        """Calcul de la distance totale entre les points d'une route"""
-        distances = [] # distances entre les points deux à deux
-        distance_route = 0 # distance totale
-        # On itère sur un zip de la liste ordre et de sa soeur décalée de 1 élément.
-        # zip() crée liste de tuples avec les points deux à deux [(point1,point2), ...] 
+        self.ordre = ordre
+        self.distance = 0
+        self.verification_formatage(ordre)
+        self.calcul_distance_route(matrice)
+
+    def __repr__(self):
+        return self.__dict__
+
+    def verification_formatage(self,ordre):
+        """Vérification du bon formatage de la liste ordre passée au init de Route"""
+        if ordre[0] != 0:
+            raise ValueError ("Le lieu d'index zéro doit être présent en première et en dernière position dans l'ordre.")
+        elif ordre[-1] != 0:
+            raise ValueError ("Le lieu d'index zéro doit être présent en première et en dernière position dans l'ordre.")
+        else :
+            set_ordre = set(ordre[1:-1]) # On compare la liste débarassée des zéro à son équivalent en set
+            if len(set_ordre) != len(ordre[1:-1]): # Si le set est plus petit, ça veut dire qu'il y a des doublons dans la liste
+                raise ValueError ("La liste ordre ne doit pas présenter d'indices en double à part le départ et l'arrivée à 0.")
+        
+    def calcul_distance_route(self, matrice):
+        """Chercher les distances dans la matrice origine-destination"""
+        distances = [] # toutes nos distances pour la route
+        distance_route = 0 # la distance totale pour la route
         for point1, point2 in zip(self.ordre, self.ordre[1:]):
-            # ajout des distances entre les points deux à deux
-            distances.append(point1.calcul_distance(point2)) 
-        distance_route = sum(distances) # somme de toutes les distances pour avoir le total
-        return distance_route
+            distances.append(matrice.loc[point1,point2])
+        distance_route = sum(distances)
+        self.distance = distance_route
